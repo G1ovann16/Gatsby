cfe19d7a8fcf3cbc6e92b0d1aef525b26714ff83
G1ovann16 <60437581+G1ovann16@users.noreply.github.com>
2020-09-12 17:00:33 +0200

    link to page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About me!!!</h1>
+    <p>lorem insum</p>
+    <Link to="/">&larr;back to home</Link>
+  </>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <h1>Hi I´m Giovanni!!</h1>
+    <p>Hello Frontend Masters!</p>
+    <Link to="/about">learn about me &rarr;</Link>
+  </>
+);
