c09c08bb4dd997e06f55123144b24ea93abf4408
G1ovann16 <60437581+G1ovann16@users.noreply.github.com>
2020-09-13 18:51:59 +0200

    error gatsby-config with plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,23 @@ module.exports = {
     title: 'Introduce to Gatsby',
     desription: 'Here we learning eith Gatsby',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: 'gatsby-mdx',
+    //   options: {
+    //     defaultLayouts: {
+    //       default: require.resolve('./src/components/layout.js'),
+    //     },
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-source-filesystem,',
+    //   options: {
+    //     name: 'posts',
+    //     patch: 'posts',
+    //   },
+    // },
+  ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,29 +2,30 @@ import React from 'react';
 import { Global, css } from '@emotion/core';
 import Helmet from 'react-helmet';
 import Header from './header';
-import useSiteMetadata from '../hooks/use-siteMetadata'; 
+import useSiteMetadata from '../hooks/use-siteMetadata';
 
-const Layout = ({ children }) => (
-    cont {title, description} = useSiteMetadata();
-  <>
-    <Global
-      styles={css`
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <Global
+        styles={css`
         *{
-            boxSizing: 'border-box';
+            box-Sizing: border-box;
             margin: 0;
             * + *{
-                marginTop: 1rem;
+                margin-Top: 1rem;
             }
         }
         html,
         body{
             margin: 0;
             color: #555; 
-            fontFamily: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, 'sans-serif';
-            fontSize: 18px;
-            lineHeight: 1.4;
+            font-Family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, 'sans-serif';
+            font-Size: 18px;
+            line-Height: 1.4;
             > div{
-                marginTop: 0; 
+                margin-Top: 0; 
             }
             h1,
             h2,
@@ -33,28 +34,29 @@ const Layout = ({ children }) => (
             h5,
             h6{
                 color: #222;
-                lineHeight: 1.1;
+                line-Height: 1.1;
                 + *{
-                    marginTop: 0.5rem:
+                    margin-Top: 0.5rem:
                 }
             }
             strong{
                 color: #222;
             }
             li{
-                marginTop: 0.25rem;
+                margin-Top: 0.25rem;
             }
         } 
      `}
-    />
-    <Helmet>
-      <html lang="en" />
-      <title>{title}</title>
-      <meta name="description" content={description} />
-    </Helmet>
-    <Header />
-    <main> {children} </main>
-  </>
-);
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main> {children} </main>
+    </>
+  );
+};
 
 export default Layout;
